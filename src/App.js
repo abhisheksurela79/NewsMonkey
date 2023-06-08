09129d1a50bf0dce5e3aa46ca6fe75e7d3dfc9f3
abhisheksurela79 <abhisheksurela79@gmail.com>
2023-06-09 03:44:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React, { Component } from 'react';
+import Navbar from './components/Navbar';
+import News from './components/News';
+import Country from './components/Country';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  apiKey = process.env.REACT_APP_NEWS_API;
+
+  constructor() {
+    super();
+    this.state = {
+      country: 'in' // Initialize the country property with a default value
+    };
+  }
+
+  handleCountryChange = (selectedCountry) => {
+    this.setState({ country: selectedCountry });
+  };
+
+  render() {
+    return (
+      <Router>
+        <Navbar onCountryChange={this.handleCountryChange} />
+        <Routes>
+          <Route path="/" exact element={<News key="general" articlesPerPage={9} country={this.state.country} category="general" apiKey={this.apiKey} />}/>
+          <Route path="/business" exact element={<News key="business" articlesPerPage={9} country={this.state.country} category="business" apiKey={this.apiKey} />}/>
+          <Route path="/entertainment" exact element={<News key="entertainment" articlesPerPage={9} country={this.state.country} category="entertainment" apiKey={this.apiKey} />}/>
+          <Route path="/health" exact element={<News key="health" articlesPerPage={9} country={this.state.country} category="health" apiKey={this.apiKey} />}/>
+          <Route path="/science" exact element={<News key="science" articlesPerPage={9} country={this.state.country} category="science" apiKey={this.apiKey} />}/>
+          <Route path="/sports" exact element={<News key="sports" articlesPerPage={9} country={this.state.country} category="sports" apiKey={this.apiKey} />}/>
+          <Route path="/technology" exact element={<News key="technology" articlesPerPage={9} country={this.state.country} category="technology" apiKey={this.apiKey} />}/>
+          {/* Add other Route components for different categories */}
+        </Routes>
+      </Router>
+    );
+  }
 }
-
-export default App;
